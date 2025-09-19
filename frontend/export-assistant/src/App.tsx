def8efe3bb6f68fa10d0687709c154c870a77256
Yuheng-Looi <looiyuheng1234@gmail.com>
2025-09-20 03:00:31 +0800

    frontend drafted

---
+++ frontend/export-assistant/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExportHelper from './components/ExportHelper';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ExportHelper />
+    </div>
+  );
+}
+
+export default App;
